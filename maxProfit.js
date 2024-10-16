69449769b8f52f48feb3cb495c8052e87b189b8e
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-10-16 14:35:51 +0530

    remove the duplicate elelemt of aaray

--- maxProfit.js
+++ maxProfit.js
@@ -26,3 +26,19 @@ function area(base, hight){
     return base * hight /2
 }
 console.log(area(2,5));
+
+// Removed Duplicate  value in arrary 
+
+function removeDuplicates(arr) {
+    let uniqueArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!uniqueArr.includes(arr[i])) {
+            uniqueArr.push(arr[i]);
+        }
+    }
+    return uniqueArr;
+}
+
+let array = [1, 2, 3, 4, 3, 2, 1];
+let uniqueArray = removeDuplicates(array);
+console.log(uniqueArray);
