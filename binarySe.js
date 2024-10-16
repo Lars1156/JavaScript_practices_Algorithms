51a33512510abb31fe446c3a1303a5391b9cbbd6
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-10-16 17:30:25 +0530

    Try to Uder stand the binary serach on array

---
+++ binarySe.js
@@ -0,0 +1,28 @@
+function binarySearch(arr,target){
+    let low = 0;
+    let high = arr.length-1
+    while(low<=high){
+        const mid = Math.floor((low + high) / 2);
+        const midValue = arr[mid];
+        if (midValue===target) {
+            return mid;
+        }else if(midValue<target){
+            low = mid+1;
+        }else{
+            high =mid-1;
+        }
+    }
+    return -1
+}
+
+const numbers = [11,3,1,67,5,6];
+numbers.sort((a,b)=>a-b);
+console.log("Sorted Array: ", numbers);
+
+const target = 1;
+const index = binarySearch(numbers,target);
+if(index != -1){
+    console.log(`Target ${target} found at index ${index}.`);    
+}else{
+    console.log(`Target ${target} not found in the array.`);
+}
