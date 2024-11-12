afb4a25cffe0fe139d9ffc737d4cc8f49200655e
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-11-12 18:35:46 +0530

    practicing the using For loop  filter ethod

--- prime.js
+++ prime.js
@@ -13,3 +13,38 @@ function isPrime(num){
 const array  = [2,3,4,5,6,7,8,9,11,12];
 const result = array.filter(isPrime);
 console.log("**Prime Number" , result);
+
+// Even and Odd Number
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+function evenNum(num){
+    return num % 2 === 0;
+}
+
+function oddNum(num){
+    return num%2 !== 0;
+}
+
+const evenNumbers = numbers.filter(evenNum);
+const oddNumbers = numbers.filter(oddNum);
+
+console.log("even Numbers" , evenNumbers);
+console.log(" Odd Numbers" , oddNumbers);
+// Using For Loop 
+let evenNumber = [];
+let oddNumber = [];
+
+for(let i = 0 ; i< numbers.length; i++){
+    if(numbers[i]%2 === 0){
+        evenNumber.push(numbers[i]);
+    }else{
+        oddNumber.push(numbers[i]);
+    }
+}
+
+console.log("**Even Numbers" , evenNumber);
+console.log("**Odd Numbers" , oddNumber);
+
+
+
+
